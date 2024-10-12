684bdcf101cd695eb44b7459e99a60ed0826e3ae
Joyce Le <jle@roycegeo.com>
2024-10-11 19:27:24 -0700

    use video as background

--- src/login/Template.tsx
+++ src/login/Template.tsx
@@ -7,6 +7,7 @@ import { useStylesAndScripts } from "keycloakify/login/Template.useStylesAndScri
 import type { I18n } from "./i18n";
 import type { KcContext } from "./KcContext";
 import logo from "./assets/Roycegeo-Logo-@2x.svg";
+import video from "./assets/background-video.mp4";
 
 export default function Template(props: TemplateProps<KcContext, I18n>) {
     const {
@@ -50,104 +51,106 @@ export default function Template(props: TemplateProps<KcContext, I18n>) {
     if (!isReadyToRender) {
         return null;
     }
-    console.log(kcContext)
 
     return (
-        <div className={kcClsx("kcLoginClass")}>
-            <div id="kc-header" className={kcClsx("kcHeaderClass")}>
-                <div id="kc-header-wrapper" className={kcClsx("kcHeaderWrapperClass")}>
-                    <img src={logo}/>
+        <>
+            <video id="video-background" src={video} autoPlay loop muted></video>
+            <div className={kcClsx("kcLoginClass")}>
+                <div id="kc-header" className={kcClsx("kcHeaderClass")}>
+                    <div id="kc-header-wrapper" className={kcClsx("kcHeaderWrapperClass")}>
+                        <img src={logo}/>
+                    </div>
                 </div>
-            </div>
 
-            <div className={kcClsx("kcFormCardClass")}>
-                <header className={kcClsx("kcFormHeaderClass")}>
-                    {(() => {
-                        const node = !(auth !== undefined && auth.showUsername && !auth.showResetCredentials) ? (
-                            <h1 id="kc-page-title"><b>{pageId === "login.ftl" ? "Sign In" : headerNode}</b></h1>
-                        ) : (
-                            <div id="kc-username" className={kcClsx("kcFormGroupClass")}>
-                                <label id="kc-attempted-username">{auth.attemptedUsername}</label>
-                                <a id="reset-login" href={url.loginRestartFlowUrl} aria-label={msgStr("restartLoginTooltip")}>
-                                    <div className="kc-login-tooltip">
-                                        <i className={kcClsx("kcResetFlowIcon")}></i>
-                                        <span className="kc-tooltip-text">{msg("restartLoginTooltip")}</span>
-                                    </div>
-                                </a>
-                            </div>
-                        );
-
-                        if (displayRequiredFields) {
-                            return (
-                                <div className={kcClsx("kcContentWrapperClass")}>
-                                    <div className={clsx(kcClsx("kcLabelWrapperClass"), "subtitle")}>
-                                        <span className="subtitle">
-                                            <span className="required">*</span>
-                                            {msg("requiredFields")}
-                                        </span>
-                                    </div>
-                                    <div className="col-md-10">{node}</div>
-                                </div>
-                            );
-                        }
-
-                        return node;
-                    })()}
-                </header>
-                <div id="kc-content">
-                    <div id="kc-content-wrapper">
-                        {/* App-initiated actions should not see warning messages about the need to complete the action during login. */}
-                        {displayMessage && message !== undefined && (message.type !== "warning" || !isAppInitiatedAction) && (
-                            <div
-                                className={clsx(
-                                    `alert-${message.type}`,
-                                    kcClsx("kcAlertClass"),
-                                    `pf-m-${message?.type === "error" ? "danger" : message.type}`
-                                )}
-                            >
-                                <div className="pf-c-alert__icon">
-                                    {message.type === "success" && <span className={kcClsx("kcFeedbackSuccessIcon")}></span>}
-                                    {message.type === "warning" && <span className={kcClsx("kcFeedbackWarningIcon")}></span>}
-                                    {message.type === "error" && <span className={kcClsx("kcFeedbackErrorIcon")}></span>}
-                                    {message.type === "info" && <span className={kcClsx("kcFeedbackInfoIcon")}></span>}
-                                </div>
-                                <span
-                                    className={kcClsx("kcAlertTitleClass")}
-                                    dangerouslySetInnerHTML={{
-                                        __html: message.summary
-                                    }}
-                                />
-                            </div>
-                        )}
-                        {children}
-                        {auth !== undefined && auth.showTryAnotherWayLink && (
-                            <form id="kc-select-try-another-way-form" action={url.loginAction} method="post">
-                                <div className={kcClsx("kcFormGroupClass")}>
-                                    <input type="hidden" name="tryAnotherWay" value="on" />
-                                    <a
-                                        href="#"
-                                        id="try-another-way"
-                                        onClick={() => {
-                                            document.forms["kc-select-try-another-way-form" as never].submit();
-                                            return false;
-                                        }}
-                                    >
-                                        {msg("doTryAnotherWay")}
+                <div className={`${kcClsx("kcFormCardClass")} kcFormCardClassDark`}>
+                    <header className={kcClsx("kcFormHeaderClass")}>
+                        {(() => {
+                            const node = !(auth !== undefined && auth.showUsername && !auth.showResetCredentials) ? (
+                                <h1 id="kc-page-title"><b>{pageId === "login.ftl" ? "Sign In" : headerNode}</b></h1>
+                            ) : (
+                                <div id="kc-username" className={kcClsx("kcFormGroupClass")}>
+                                    <label id="kc-attempted-username">{auth.attemptedUsername}</label>
+                                    <a id="reset-login" href={url.loginRestartFlowUrl} aria-label={msgStr("restartLoginTooltip")}>
+                                        <div className="kc-login-tooltip">
+                                            <i className={kcClsx("kcResetFlowIcon")}></i>
+                                            <span className="kc-tooltip-text">{msg("restartLoginTooltip")}</span>
+                                        </div>
                                     </a>
                                 </div>
-                            </form>
-                        )}
-                        {socialProvidersNode}
-                        {displayInfo && (
-                            <div id="kc-info" className={kcClsx("kcSignUpClass")}>
-                                <div id="kc-info-wrapper" className={kcClsx("kcInfoAreaWrapperClass")}>
-                                    {infoNode}
+                            );
+
+                            if (displayRequiredFields) {
+                                return (
+                                    <div className={kcClsx("kcContentWrapperClass")}>
+                                        <div className={clsx(kcClsx("kcLabelWrapperClass"), "subtitle")}>
+                                            <span className="subtitle">
+                                                <span className="required">*</span>
+                                                {msg("requiredFields")}
+                                            </span>
+                                        </div>
+                                        <div className="col-md-10">{node}</div>
+                                    </div>
+                                );
+                            }
+
+                            return node;
+                        })()}
+                    </header>
+                    <div id="kc-content">
+                        <div id="kc-content-wrapper">
+                            {/* App-initiated actions should not see warning messages about the need to complete the action during login. */}
+                            {displayMessage && message !== undefined && (message.type !== "warning" || !isAppInitiatedAction) && (
+                                <div
+                                    className={clsx(
+                                        `alert-${message.type}`,
+                                        kcClsx("kcAlertClass"),
+                                        `pf-m-${message?.type === "error" ? "danger" : message.type}`
+                                    )}
+                                >
+                                    <div className="pf-c-alert__icon">
+                                        {message.type === "success" && <span className={kcClsx("kcFeedbackSuccessIcon")}></span>}
+                                        {message.type === "warning" && <span className={kcClsx("kcFeedbackWarningIcon")}></span>}
+                                        {message.type === "error" && <span className={kcClsx("kcFeedbackErrorIcon")}></span>}
+                                        {message.type === "info" && <span className={kcClsx("kcFeedbackInfoIcon")}></span>}
+                                    </div>
+                                    <span
+                                        className={kcClsx("kcAlertTitleClass")}
+                                        dangerouslySetInnerHTML={{
+                                            __html: message.summary
+                                        }}
+                                    />
                                 </div>
-                            </div>
-                        )}
+                            )}
+                            {children}
+                            {auth !== undefined && auth.showTryAnotherWayLink && (
+                                <form id="kc-select-try-another-way-form" action={url.loginAction} method="post">
+                                    <div className={kcClsx("kcFormGroupClass")}>
+                                        <input type="hidden" name="tryAnotherWay" value="on" />
+                                        <a
+                                            href="#"
+                                            id="try-another-way"
+                                            onClick={() => {
+                                                document.forms["kc-select-try-another-way-form" as never].submit();
+                                                return false;
+                                            }}
+                                        >
+                                            {msg("doTryAnotherWay")}
+                                        </a>
+                                    </div>
+                                </form>
+                            )}
+                            {socialProvidersNode}
+                            {displayInfo && (
+                                <div id="kc-info" className={kcClsx("kcSignUpClass")}>
+                                    <div id="kc-info-wrapper" className={kcClsx("kcInfoAreaWrapperClass")}>
+                                        {infoNode}
+                                    </div>
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
